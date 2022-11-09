8eba8f296cc32374e6cdee899ecf7b3beb14abe9
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-11-09 17:41:13 -0300

    Removida a div que englobava o BrowserRouter no arquivo App.js

--- src/App.js
+++ src/App.js
@@ -12,19 +12,17 @@ import Search from './pages/Search';
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/" component={ Login } />
-            <Route path="/search" component={ Search } />
-            <Route path="/album/:id" component={ Album } />
-            <Route path="/favorites" component={ Favorites } />
-            <Route exact path="/profile" component={ Profile } />
-            <Route exact path="/profile/edit" component={ ProfileEdit } />
-            <Route path="*" component={ NotFound } />
-          </Switch>
-        </BrowserRouter>
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route path="/search" component={ Search } />
+          <Route path="/album/:id" component={ Album } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route exact path="/profile/edit" component={ ProfileEdit } />
+          <Route path="*" component={ NotFound } />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
