7a467cedf04430d6d9cc9a4432a4ca35a653b93e
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-11-09 17:51:37 -0300

    Substituindo código da barra de navegação, pelo componente contendo os links utilizados por ela

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-
 import { getUser } from '../services/userAPI';
-
 import Loading from './Loading';
+import NavBar from './HeaderLinks';
+import '../styles/Header.css';
 
 class Header extends React.Component {
   constructor() {
@@ -39,36 +38,7 @@ class Header extends React.Component {
               <p className="header-user" data-testid="header-user-name">{user.name}</p>
             </section>
             <nav className="header-navigation">
-              <Link
-                className="header-link"
-                data-testid="link-to-search"
-                to="/search"
-              >
-                <span className="header-icon">
-                  <ion-icon name="search-circle-outline" />
-                </span>
-                <span className="header-text-nav">Pesquisa</span>
-              </Link>
-              <Link
-                className="header-link"
-                data-testid="link-to-favorites"
-                to="/favorites"
-              >
-                <span className="header-icon">
-                  <ion-icon name="heart-outline" />
-                </span>
-                <span className="header-text-nav">Favoritas</span>
-              </Link>
-              <Link
-                className="header-link"
-                data-testid="link-to-profile"
-                to="/profile"
-              >
-                <span className="header-icon">
-                  <ion-icon name="person-outline" />
-                </span>
-                <span className="header-text-nav">Perfil</span>
-              </Link>
+              <NavBar />
             </nav>
           </>
         )}
